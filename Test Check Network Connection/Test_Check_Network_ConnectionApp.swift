be82d2715d637ebac57a913cb6a3d17cb55c7ee6
codebyjames <codebyjames@gmail.com>
2021-11-20 18:11:05 -0500

    Initial Commit

---
+++ Test Check Network Connection/Test_Check_Network_ConnectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Check_Network_ConnectionApp.swift
+//  Test Check Network Connection
+//
+//  Created by James Loboda on 11/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Test_Check_Network_ConnectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
